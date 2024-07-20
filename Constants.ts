e5369c0a214d1bda47a417559469497677e84365
Ricardo Carrillo <riccardo.carrillo7@gmail.com>
2024-07-20 12:17:43 -0500

    Modified constants file

--- Constants.ts
+++ Constants.ts
@@ -1,3 +1,18 @@
 export const BIRDDOG_NFT_NAME = 'BirdDogNFT';
 export const BIRDDOG_NFT_SYMBOL = 'BDOG';
 export const IPFS_HASH = 'QmR1eRsCqpMsHq9KaGHBGRw4YWjhj9kCwyeozCoH7am2Vb';
+
+const mintAllocationAddressTA = '0x23a3e11A7b66Ed7019144dB4557E20278E63b7d8';
+const mintAllocationAddressBrokenByte = '0x55B554EFaDdB6a229A3785cc535779E54be5308c';
+const mintAllocationAddressJoeRahn = '0xa7470329c95A45a3EBBF58b2368ba996281bb0eA';
+const mintAllocationAddressBirddogTreasury = '0x716696054A38819cf03f0e18347765faEbeA3A2D';
+const mintAllocationAddressZlurpeeTreasury = '0x7B54A3EB099B25913cae7593A531774f7b4F9A36';
+
+export const WITHDRAWAL_ADDRESSES = [
+  mintAllocationAddressBirddogTreasury,
+  mintAllocationAddressZlurpeeTreasury,
+  mintAllocationAddressTA,
+  mintAllocationAddressBrokenByte,
+  mintAllocationAddressJoeRahn,
+];
+export const WITHDRAWAL_ALLOCATION_PERCENTAGE_NUMERATORS = [3500, 3500, 1000, 1000, 1000];
