d923ca6508df51f930a07e96f68201c52aebd82c
Ricardo Carrillo <riccardo.carrillo7@gmail.com>
2024-07-21 10:34:44 -0500

    More work to prep for testnet/main net launch

--- contracts/ignition/modules/BirddogNFTMainnet.ts
+++ contracts/ignition/modules/BirddogNFTMainnet.ts
@@ -1,22 +1,38 @@
 import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
-import { IPFS_HASH, BIRDDOG_NFT_NAME, BIRDDOG_NFT_SYMBOL } from '../../../Constants';
+import {
+  IPFS_HASH,
+  BIRDDOG_NFT_NAME,
+  BIRDDOG_NFT_SYMBOL,
+  BROKENBYTE_ENGINEERING_DEPLOYER,
+  ARTIST,
+  WITHDRAWAL_ADDRESSES,
+  WITHDRAWAL_ALLOCATION_PERCENTAGE_NUMERATORS,
+  ROYALTY_MULTISIG,
+} from '../../../Constants';
 
 const BirddogNFTModule = buildModule('BirddogNFT', (m) => {
-  const ownerAddress = '0x566bD9Df983BfEFf836A2C3b644553e6A80850Fa'; // BrokenByte Engineering Deployer;;
-  const artistAddress = '0xa7470329c95A45a3EBBF58b2368ba996281bb0eA';
-  const royaltyMultisigAddress = '0x073418FF5773Dd93c03C04E85Cb0Abd66796F866';
-
   const initBaseURI = `ipfs://${IPFS_HASH}/`;
 
-  const isTestnet: Boolean = false;
+  /**
+   * constructor(
+    string memory _name,
+    string memory _symbol,
+    address owner,
+    address artist,
+    address royaltyMultisig,
+    address[] memory _withdrawAddresses,
+    uint256[] memory _withdrawPercentageNumerators,
+    string memory _initBaseURI
+  )
+   */
 
   // 1. Deploy contract
   const birddogNFT = m.contract('BirddogNFT', [
     BIRDDOG_NFT_NAME, // name
     BIRDDOG_NFT_SYMBOL, // symbol
-    ownerAddress, // owner
-    artistAddress, // artist
-    royaltyMultisigAddress, // royaltyMultisig
+    BROKENBYTE_ENGINEERING_DEPLOYER, // owner
+    ARTIST, // artist
+    ROYALTY_MULTISIG, // royaltyMultisig
     initBaseURI, // initBaseURI
   ]);
 
