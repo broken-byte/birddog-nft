7fc1fa34fb0c5b81af6b8d4daa987be525035e24
Ricardo Carrillo <riccardo.carrillo7@gmail.com>
2024-07-22 15:28:03 -0600

    Edited the name

--- Constants.ts
+++ Constants.ts
@@ -1,4 +1,4 @@
-export const BIRDDOG_NFT_NAME = 'BirdDogNFT';
+export const BIRDDOG_NFT_NAME = 'BirdDog: The NFT';
 export const BIRDDOG_NFT_SYMBOL = 'BDOG';
 
 export const IPFS_HASH = 'QmR1eRsCqpMsHq9KaGHBGRw4YWjhj9kCwyeozCoH7am2Vb';
