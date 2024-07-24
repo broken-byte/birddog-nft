c6182f6ae7c3077df96edd3120027aed126cf1f6
Ricardo Carrillo <riccardo.carrillo7@gmail.com>
2024-07-24 16:47:40 -0600

    Set up hardhat gas reporter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,14 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
+import 'hardhat-gas-reporter';
 
 const ALCHEMY_BASE_MAINNET_API_KEY: string = process.env.ALCHEMY_API_KEY!!;
 const BASE_ENGINEERING_DEPLOYER_PRIVATE_KEY: string =
   process.env.BASE_ENGINEERING_DEPLOYER_PRIVATE_KEY!!;
 const ALCHEMY_BASE_SEPLOYIA_API_KEY: string = process.env.ALCHEMY_BASE_SEPOLIA_API_KEY!!;
 const BASESCAN_BASED_WHALE_API_KEY: string = process.env.BASESCAN_BASED_WHALE_API_KEY!!;
+const COINMARKETCAP_API_KEY: string = process.env.COINMARKETCAP_API_KEY!!;
+const ETHERSCAN_API_KEY: string = process.env.ETHERSCAN_API_KEY!!;
 
 const config: HardhatUserConfig = {
   solidity: '0.8.24',
@@ -13,7 +16,7 @@ const config: HardhatUserConfig = {
     hardhat: {
       forking: {
         url: `https://base-mainnet.g.alchemy.com/v2/${ALCHEMY_BASE_MAINNET_API_KEY}`,
-        blockNumber: 15972484,
+        blockNumber: 17535784,
       },
     },
     // Base Sepolia (testnet)
@@ -36,6 +39,15 @@ const config: HardhatUserConfig = {
     // Disabled by default
     // Doesn't need an API key
     enabled: true,
+  },
+  gasReporter: {
+    currency: 'USD',
+    L1: 'ethereum',
+    L2: 'base',
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    L1Etherscan: ETHERSCAN_API_KEY,
+    L2Etherscan: BASESCAN_BASED_WHALE_API_KEY,
+    darkMode: true,
   },
 };
 
