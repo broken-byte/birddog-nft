e6c6a664c3274494e3210964344d1719c680e225
Ricardo Carrillo <riccardo.carrillo7@gmail.com>
2024-07-27 12:17:30 -0600

    cleaned up airdrop parse script

--- airdrop/parseCsvAirdropList.ts
+++ airdrop/parseCsvAirdropList.ts
@@ -35,38 +35,3 @@ export const parseCSV = (filePath: string): Promise<AirdropParticipant[]> => {
     });
   });
 };
-
-const filePath = './airdrop/airdrop.csv';
-let participants: string[] = [];
-let mintAmounts: number[] = [];
-parseCSV(filePath)
-  .then((participantData) => {
-    participantData.map((participantDatum) => {
-      participants.push(participantDatum.Addresses);
-      mintAmounts.push(participantDatum.MintAmount);
-    });
-
-    console.log('Participants:', participants);
-    console.log('Mint Amounts:', mintAmounts);
-  })
-  .catch((error) => {
-    console.error('Error parsing CSV:', error);
-  });
-
-// const filePath = './airdrop/airdrop.csv';
-// parseCSV(filePath)
-//   .then((participants) => {
-//     console.log('Parsed Participants:', participants);
-//     console.log('==============================');
-//     console.log('Count: ', participants.length);
-
-//     let sum = 0;
-//     participants.map((participant) => {
-//       sum += participant.MintAmount;
-//     });
-
-//     console.log('Total Mint Amount: ', sum);
-//   })
-//   .catch((error) => {
-//     console.error('Error parsing CSV:', error);
-//   });
